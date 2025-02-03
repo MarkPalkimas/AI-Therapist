0dd9a7e1c5e4f982de4d1a6ebd42ad420e377add
Mark <85582966+MarkPalkimas@users.noreply.github.com>
2025-02-03 18:50:16 -0500

    Delete server directory

--- server/routes/therapy.js
+++
@@ -1,9 +0,0 @@
-// server/routes/therapy.js
-const express = require('express');
-const router = express.Router();
-const therapyController = require('../controllers/therapyController');
-
-router.post('/analyze', therapyController.analyze);
-router.post('/feedback', therapyController.feedback);
-
-module.exports = router;
