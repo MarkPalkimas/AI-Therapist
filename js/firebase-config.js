fdb3c396a2296c9b76268729e4b3218777487219
Mark <85582966+MarkPalkimas@users.noreply.github.com>
2025-02-03 18:59:57 -0500

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// js/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCu4swb2WAFFMq6MgaB3IUJxPm6l3oCBOU",
+  authDomain: "therapist-ai-844dc.firebaseapp.com",
+  projectId: "therapist-ai-844dc",
+  storageBucket: "therapist-ai-844dc.firebasestorage.app",
+  messagingSenderId: "296097580333",
+  appId: "1:296097580333:web:28efbeb8b27aa3b88bbfa4",
+  measurementId: "G-BR3HGR80H8"
+};
