3bcc4f62a687b1019ce087090030d0a4df6ab382
Mark <85582966+MarkPalkimas@users.noreply.github.com>
2025-02-03 19:20:40 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,8 @@
-// js/firebase-config.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyCu4swb2WAFFMq6MgaB3IUJxPm6l3oCBOU",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "therapist-ai-844dc.firebaseapp.com",
   projectId: "therapist-ai-844dc",
-  storageBucket: "therapist-ai-844dc.firebasestorage.app",
+  storageBucket: "therapist-ai-844dc.appspot.com",
   messagingSenderId: "296097580333",
   appId: "1:296097580333:web:28efbeb8b27aa3b88bbfa4",
   measurementId: "G-BR3HGR80H8"
