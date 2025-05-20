5e62963fc2944af0d16b85086e52f125efeb6ccd
Mark <85582966+MarkPalkimas@users.noreply.github.com>
2025-05-20 13:13:47 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,9 @@
-export const firebaseConfig = {
+// src/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "therapist-ai-844dc.firebaseapp.com",
   projectId: "therapist-ai-844dc",
@@ -7,3 +12,11 @@ export const firebaseConfig = {
   appId: "1:296097580333:web:28efbeb8b27aa3b88bbfa4",
   measurementId: "G-BR3HGR80H8"
 };
+
+// Initialize
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+signInAnonymously(auth).catch(console.error);
+const database = getDatabase(app);
+
+export { database };
