2709c74662fdafc0ca807c7d0c3b2120a05f9630
Mark <85582966+MarkPalkimas@users.noreply.github.com>
2025-05-22 12:46:19 -0400

    Update emotionDataset.js

--- Src/modules/emotionDataset.js
+++ Src/modules/emotionDataset.js
@@ -1,1 +1,11 @@
+export default {
+  joy:     ["happy","joy","glad","delighted","excited"],
+  sadness: ["sad","unhappy","down","tear","depressed"],
+  anger:   ["angry","mad","furious","irritated","annoyed"],
+  fear:    ["scared","afraid","fear","terrified","anxious"],
+  surprise:["surprised","wow","shocked","astonished"],
+  love:    ["love","adore","cherish","fond"],
+  anxiety: ["anxious","worried","nervous","tense"],
+  disgust: ["disgusted","revolted","nauseated"]
+};
 
